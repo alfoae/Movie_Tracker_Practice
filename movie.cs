a406e91e70a4a787c5f331dce32bb270ea323c30
alfoae <vzhe.zanjato@gmail.com>
2026-04-12 21:35:38 +0300

    Добавьте файлы проекта.

---
+++ movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public bool IsWatched { get; set; }
+
+        public Movie() { }
+
+        public Movie(string title, int year, string genre, bool isWatched = false)
+        {
+            Title = title;
+            Year = year;
+            Genre = genre;
+            IsWatched = isWatched;
+        }
+    }
+}
